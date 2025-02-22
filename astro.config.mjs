a9bf3e20708cf57238c15488e597cde9af858e79
Altismann <87611738+Altismann@users.noreply.github.com>
2025-02-22 16:53:57 +0000

    fd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,11 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+
+{
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: 'file'
+  }
+}
